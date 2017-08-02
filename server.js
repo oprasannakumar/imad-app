27c754478e14772ff60743eb43402468331656ef
oprasannakumar <30617219+oprasannakumar@users.noreply.github.com>
2017-08-02 20:36:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,8 +80,6 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    //articleName==Articleone
-    //articles[articleName]=={} content object for article one
   var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
