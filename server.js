fc2fc10384990f5b4656500b5d4bd017958d5b65
oprasannakumar <30617219+oprasannakumar@users.noreply.github.com>
2017-08-02 17:12:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/articleone.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 
+app.get('/articletwo.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
