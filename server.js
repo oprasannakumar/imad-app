db18a18484d1e5f0fe9e3b591efa33b08e3589c5
oprasannakumar <30617219+oprasannakumar@users.noreply.github.com>
2017-08-02 19:38:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title :'Articleone | Prasanna Kumar',
+    heading:'Articleone',
+    date:'Aug 2, 2017',
+    content: `
+            <p>
+                Hai guys this is my first web site designed by my own by learning some basics that have been learnt from the nptel.
+            </p>
+            <p>
+                Hope you will appreciate me. Soner I will design my own webapp
+            </p>
+            <p>
+                I am very glad to say that I am making my own web app.
+            </p>`
+};
+function createTemplate (data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+        var htmlTemplate=`
+        <html>
+         <head>
+             <title>
+                 ${title}
+             </title>
+             <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <link href="/ui/style.css" rel="stylesheet" />
+         </head>
+              <div class="container">
+                <div>
+                    <a href="/">home</a>
+                    <a href="/articletwo.html">articletwo</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>  
+              </div>
+                
+        </html>
+        `;
+        return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/articleone.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
+  res.send(createTemplate(ArticleOne))
 });
 
 app.get('/articletwo.html', function (req, res) {
