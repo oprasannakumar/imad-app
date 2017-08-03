fa1e86d25450cdb5afdd09616266922c4b412cc8
oprasannakumar <30617219+oprasannakumar@users.noreply.github.com>
2017-08-03 13:33:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var img=document.getElementById('pressu');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+5;
+    marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
 }
 //var marginLeft=margin-Left;
